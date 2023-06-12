94777c8c95e884b4b1b1a85fd1fe679cb216fe23
Alexisdks <i202114481@cibertec.edu.pe>
2023-06-12 17:22:04 -0500

    Practica Finalizada

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./solid/02.open-close-a";
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`;
